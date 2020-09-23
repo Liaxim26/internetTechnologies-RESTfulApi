511a23123d241616547557d31c8f2489ea825d08
Liaxim26 <66952084+Liaxim26@users.noreply.github.com>
2020-09-24 00:24:59 +0300

    просмотр меню и одного блюда

--- functions.php
+++ functions.php
@@ -4,4 +4,6 @@ function Error($p1, $p2) {
 	exit('{"error;"'.$p1.', "text:"'.$p2.'"}');
 }
 
+
+
  ?>
--- index.php
+++ index.php
@@ -4,11 +4,18 @@
 $json = json_decode(file_get_contents('users.json', true));
 header('Content-type: json/application');
 require 'functions.php';
-$Module = $_GET['url_param'];
+$Url = $_GET['url_param'];
+$params = explode('/', $Url);
 
+$Module = $params[0];
+$id = $params[1];
 
-
-if ($Module === 'dishes') {
+if ($Module == 'dishes') {
+	if(isset($id)){
+		echo json_encode($json[$id]);
+	} else {
+		echo json_encode($json);
+	}
 		
 }
 
