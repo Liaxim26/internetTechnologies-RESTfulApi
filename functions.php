801e95db5f1f1f0f75edd34137760f83e9de13e7
Liaxim26 <66952084+Liaxim26@users.noreply.github.com>
2020-09-24 17:35:11 +0300

    added "addDishes" function

--- functions.php
+++ functions.php
@@ -5,6 +5,18 @@ function Error($p1, $p2) {
 	exit('{"status code;"'.$p1.', "ctatus text:"'.$p2.'"}');
 }
 
+function addDishes($post, $json) {
+	$taskList = $json;
+	unset($json);
+	foreach ($post as $key => $value) {
+		$taskLists[$key] = $value; 
+	}
+	$taskList[] = $taskLists;
+	print_r($taskList);
+	file_put_contents('users.json',json_encode($taskList));     
+	unset($taskList); 
+}
+
 
 
  ?>
--- index.php
+++ index.php
@@ -1,25 +1,33 @@
 <?php
 
-
 $json = json_decode(file_get_contents('users.json', true));
 header('Content-type: json/application');
 require 'functions.php';
 $Url = $_GET['url_param'];
 $params = explode('/', $Url);
+$method = $_SERVER['REQUEST_METHOD'];
 
 $Module = $params[0];
 $id = $params[1];
 
-if ($Module == 'dishes') {
-	if(isset($id)){
-		$res = json_encode($json[$id]);
-		if ($res == 'null') echo Error('404', 'Dish not found');
-		else echo $res;
-	} else {
-		echo json_encode($json);
+if ($method == 'GET') {
+	if ($Module == 'dishes') {
+		if(isset($id)){
+			$res = json_encode($json[$id]);
+			if ($res == 'null') echo Error('404', 'Dish not found');
+			else echo $res;
+		} else echo json_encode($json);		
 	}
-		
+} elseif ($method == 'POST') {
+	if ($Module == 'dishes') {
+		if(isset($id)){
+			print_r('qwe');
+		} else addDishes($_POST, $json);
+	}
 }
+
+
+
 
 
 
